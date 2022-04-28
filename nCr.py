f0e48889df96549fb64b8fa064d8a39920e99c77
ClassErased <91396239+ClassErased@users.noreply.github.com>
2022-04-28 19:15:46 +0100

    Update nCr.py

--- nCr.py
+++ nCr.py
@@ -2,16 +2,20 @@ import math
 import time
 import sys
 
+#Welcome message
 print("Program to perform nCr calculation for combinations")
+
 #Define selection pool
-n = input("What is the number of possible selections?: ")
-n = int(n)
+n = int(input("What is the number of possible selections?: "))
+
 #Define selection limit
-r = input("How many can you choose?: ")
-r = int(r)
+r = int(input("How many can you choose?: "))
+
 #This method ignores repeats as it is assumed users will not repeat words
 print("Calculating...")
-#Print amount of possiblities
+
+#Perform nCr calculation then print amount of possiblities.
+#Exit the program after 10 seconds
 print (math.comb(n, r)) 
 time.sleep(10)
 print("Exiting program...")
